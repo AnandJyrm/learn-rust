29cec3d3d7a3b7ee8075acb671fce1920ea3577c
Anand Jayaram <anandja@cisco.com>
2023-07-03 11:46:31 -0700

    formatting traits

---
+++ hello-world/fmt_trait.rs
@@ -0,0 +1,19 @@
+use fmt::Formatter;
+use std::fmt;
+
+fn main() {
+    struct Structure(i32);
+    impl fmt::Debug for Structure {
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            write!(f, "hello this is Structure's Debug: {}", self.0)
+        }
+    }
+    impl fmt::Display for Structure {
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            write!(f, "hello this is Structure's Display: {}", self.0)
+        }
+    }
+
+    println!("Debug: {:?}", Structure(3));
+    println!("Display: {}", Structure(3));
+}
